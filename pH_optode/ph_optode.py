d2707ec7682a51ebf652947276b2170261b8ef95
Louise Delaigue <louisedelaigue@gmail.com>
2020-08-04 11:54:55 +0200

    added loop for table extraction

--- pH_optode/ph_optode.py
+++ pH_optode/ph_optode.py
@@ -6,7 +6,13 @@ import os
 db = pd.read_excel('.\lab_cs_instruments_config_mock.xlsx',
                    skiprows=[1])
 
-#%% create list and loop
+#%% create list of files we want to keep
 file_list = [file for file in os.listdir('.\data') if 
                   '_'.join(file.split('_')[2:]) in db.pH_optN.values]
 
+#%% create loop to extract data
+data = {} # tell python this is an empty dict so we can put the tables in
+for file in file_list:
+    fname = ".\data\{}\{}.txt".format(file,file)
+    data[file] = pd.read_table(fname, skiprows=20)
+    
