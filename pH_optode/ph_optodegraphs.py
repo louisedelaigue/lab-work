c2e4c29e3a1369b3ee32a2ba8f2cf9e52535d218
Louise Delaigue <louisedelaigue@gmail.com>
2020-08-11 14:06:57 +0200

    Update ph_optodegraphs.py

--- pH_optode/ph_optodegraphs.py
+++ pH_optode/ph_optodegraphs.py
@@ -1,2 +1,21 @@
-# -*- coding: utf-8 -*-
+#%% PLOTTING
+#%% plotting the regression with seaborn
+for file in file_list:
+    fig, ax = plt.subplots()
+    sns.regplot(data[file].sec, data[file].pH, fit_reg=True, x_ci="sd")
+    
+#%% scatter each plot individually
+for file in file_list:
+    data[file].plot.scatter("sec", "pH")
 
+#%% plot line each file on same plot
+fig, ax = plt.subplots()
+for file in file_list:
+    data[file].plot("sec", "pH", ax=ax)
+    
+#%% jointplot of averages distribution
+fig, ax = plt.subplots()
+sns.despine(left=True)
+d = avg.average_pH
+#sns.distplot(d, kde=False, color="b", ax=ax)
+sns.jointplot(d,d, kind="hex", color="#4CB391")
