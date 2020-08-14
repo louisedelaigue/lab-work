f9bc5e76eac2bb49f7770ed1dd60e8b58c29e493
Louise Delaigue <louisedelaigue@gmail.com>
2020-08-14 11:15:50 +0200

    added std err and intercept for slope 0

--- pH_optode/ph_optode.py
+++ pH_optode/ph_optode.py
@@ -53,12 +53,16 @@ data_c = data.copy()
 
 # create table to hold results
 results = pd.DataFrame({"filename":file_list})
+results["pH_raw_mean"] = np.nan
+results["pH_raw_median"] = np.nan
 results["pH_last2min_mean"] = np.nan
 results["pH_last2min_median"] = np.nan
 results["slope"] = np.nan
 results["lowest_ix"] = np.nan
 results["pH_s0_mean"] = np.nan
 results["pH_s0_median"] = np.nan
+results["pH_s0_stderr"] = np.nan
+results["pH_s0_intercept"] = np.nan
 
 # compute mean and median of last 2min of measurements using all datapoints
 for file in file_list:
@@ -96,19 +100,14 @@ for file in file_list: #[file_list[2]]:
     
     # calculate the mean
     mean = data[file].pH[lowest_ix:].mean()
+         
+    # store the raw mean in df
+    results.loc[results.filename==file,
+                "pH_raw_mean"] = data[file].pH.mean()
     
-    # create 1 fig per sample w/ 2 subplots
-    fig, ax = plt.subplots(2,1)
-    data[file].plot.scatter('sec','slope_here', ax=ax[0])
-    ax[0].set_xlim([0, 600])
-    ax[0].set_ylim([data[file].slope_here.min(),
-                 data[file].slope_here.max()])
-    data[file].plot.scatter('sec', 'pH', ax=ax[1])
-    data[file].loc[lowest_ix:].plot.scatter('sec','pH', ax=ax[1], c='r')
-    ax[1].axhline(mean, c='r')
-    ax[1].set_xlim([0, 600])
-    ax[1].set_ylim([data[file].pH.min(),
-                 data[file].pH.max()])
+    # store the raw mean in df
+    results.loc[results.filename==file,
+                "pH_raw_median"] = data[file].pH.median()
     
     # store index of lowest abs slope in df
     results.loc[results.filename==file,
@@ -125,3 +124,51 @@ for file in file_list: #[file_list[2]]:
     # store the median in df
     results.loc[results.filename==file,
                 "pH_s0_median"] = data[file].pH[lowest_ix:].median()
+    
+    # store the std_err for slope 0 in df
+    results.loc[results.filename==file,
+                "pH_s0_stderr"] = stats.linregress(data[file].sec[lowest_ix:],
+                                                   data[file].pH[lowest_ix:])[4]
+    
+    # store the intercept for slope 0 in df
+    results.loc[results.filename==file,
+                "pH_s0_intercept"] = stats.linregress(data[file].sec[lowest_ix:],
+                                                   data[file].pH[lowest_ix:])[1]
+    
+    # create 1 fig per sample w/ 2 subplots
+    fig, ax = plt.subplots(2,1, figsize=(8, 6))
+    fig.tight_layout(pad=3, w_pad=2.0, h_pad=1.0)
+    plt.rcParams.update({'font.size': 15})
+    
+    # subplot 1
+    data[file].plot.scatter('sec','slope_here', c='xkcd:electric blue',
+                            ax=ax[0])
+    ax[0].set_xlim([0, 600])
+    ax[0].set_ylim([data[file].slope_here.min(),
+                 data[file].slope_here.max()])
+    ax[0].set_xlabel("")
+    ax[0].set_ylabel('Slope')
+    ax[0].set_title('Sample '+str(file.split('_')[2:]))
+    ax[0].yaxis.labelpad=10.0
+    
+    #subplot 2    
+    data[file].plot.scatter('sec', 'pH', c='xkcd:electric blue', ax=ax[1])
+    data[file].loc[lowest_ix:].plot.scatter('sec','pH',  c='r',
+                                            ax=ax[1])
+    ax[1].axhline(mean, c='r')
+    ax[1].set_xlim([0, 600])
+    ax[1].set_ylim([data[file].pH.min(),
+                 data[file].pH.max()])
+    ax[1].set_xlabel('Time (sec)')
+    ax[1].set_ylabel('pH')
+    ax[1].xaxis.labelpad=10.0
+    ax[1].yaxis.labelpad=10.0
+    
+    # save image in high quality, png format
+    #filen = './results/{}.png'.format(file)
+    #plt.savefig(filen, dpi=300)
+    plt.show()
+    
+    # save results as text file
+    results.to_csv('./results/results.txt', index=None, sep=' ')
+    
