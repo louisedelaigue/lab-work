cc8e36300b01ba9b6b29a76c33a8b2d5d57f491c
Louise Delaigue <louisedelaigue@gmail.com>
2020-09-08 10:09:41 +0200

    created script for quick pyco2sys

---
+++ pH_optode/quick_pyco2sys.py
@@ -0,0 +1,11 @@
+import PyCO2SYS as pyco2
+CRM = pyco2.CO2SYS_nd(2212.00, 2012.59, 1, 2, 
+                      salinity=33.525,
+                      temperature_out=20,
+                      pressure_out=0,
+                      total_phosphate=0.42,
+                      total_silicate=3.3,
+                      opt_pH_scale=1,
+                      opt_k_carbonic=16,
+                      opt_total_borate=1
+                      )
