645bc5fff1b1c2fe246c59804944bbd0334aad87
Louise Delaigue <louisedelaigue@gmail.com>
2020-09-16 16:23:25 +0200

    built scatter plot for pH optode comparing

--- pH_optode/compare_optode_rws.py
+++ pH_optode/compare_optode_rws.py
@@ -2,41 +2,63 @@
 # import toolbox
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib
 
 # import data 
-datac = pd.read_csv('./data_rws/result_RWS_01_11_comparison.csv',
+datac = pd.read_csv('./results/rws_1609_comparison.csv',
                    skiprows=[1])
-datac = datac.drop(datac.tail(1).index) # systematically drops last CRM
+#datac = datac.drop(datac.tail(1).index) # systematically drops last CRM
+
+# replace nan by "CRM" for station/bottle columns
+datac.bottle.replace(np.nan, 'CRM', regex=True, inplace=True)
+datac.station.replace(np.nan, 'CRM', regex=True, inplace=True)
+
+# create name column
+datac.bottle = datac.bottle.astype(str)
+datac["name"] = datac["station"] +"_"+ datac["bottle"]
 
 # create new columns to store diff
 datac["diff_opt_calc"] = np.nan
 datac["diff_opt_spec"] = np.nan
 datac["diff_opt_vindta"] = np.nan
 
-datac["diff_opt_calc"] = np.abs(datac["pH_s0_mean"] - datac["pH_calc12_total_20"])
-datac["diff_opt_spec"] = np.abs(datac["pH_s0_mean"] - datac["pH_spectro_total_20"])
-datac["diff_opt_vindta"] = np.abs(datac["pH_s0_mean"] - datac["pH_vindta_total_20"])
+datac["diff_opt_calc"] = datac["pH_s0_mean"] - datac["pH_calc12_total_20"]
+datac["diff_opt_spec"] = datac["pH_s0_mean"] - datac["pH_spectro_total_20"]
+datac["diff_opt_vindta"] = datac["pH_s0_mean"] - datac["pH_vindta_total_20"]
 
-# plot differences
-fig, ax = plt.subplots(figsize=(15, 6), dpi=300)
-plt.rcParams.update({'font.size': 15})
+# remove CRM and outlier
+indexCRM = datac[datac["name"] == "CRM_CRM"].index
+datac.drop(indexCRM, inplace = True)
+indexO = datac[datac["name"] == "WALCRN20_2020005960.0"].index
+datac.drop(indexO, inplace = True)
 
-ax.plot(datac.station, datac.diff_opt_calc, label="pH_opt vs. pH_calc",
-        marker='.', markersize=10)
-ax.plot(datac.station, datac.diff_opt_spec, label="pH_opt vs. pH_spec",
-        marker='.', markersize=10)
-ax.plot(datac.station, datac.diff_opt_vindta, label="pH_opt vs. pH_vindta",
-        marker='.', markersize=10)
+#%% scatterplot
+f, ax = plt.subplots(1, 3, figsize=(20, 6.5))
+sns.set_style("darkgrid")
+sns.set_context("paper")
+sns.set(font="Verdana")
+sns.despine(f, left=True, bottom=True)
 
-ax.legend(loc='best', facecolor="white", framealpha=1)
+# line through origin
+x = [7.70, 7.70]
+y = [8.20, 8.20]
+x_values = [x[0], y[0]]
+y_values = [x[1], y[1]]
 
-ax.axhline(0, c='k', linewidth=0.8)
-ax.set_ylim(-0.05, 0.25)
+sns.lineplot(x=x_values, y=y_values, ax=ax[0], color="black")
+sns.lineplot(x=x_values, y=y_values, ax=ax[1], color="black")
+sns.lineplot(x=x_values, y=y_values, ax=ax[2], color="black")
 
-ax.set_ylabel("Difference in pH methods")
-ax.set_xlabel("Sample")
-plt.xticks(fontsize=13)
-
+sns.scatterplot(x="pH_s0_mean", y="pH_calc12_total_20", 
+                hue="diff_opt_calc",  s=120,
+                palette="ch:r=-.2,d=.3_r", data=datac, ax=ax[0])
+sns.scatterplot(x="pH_s0_mean", y="pH_spectro_total_20", 
+                hue="diff_opt_spec", s=120,
+                palette="ch:r=-.2,d=.3_r", data=datac, ax=ax[1])
+sns.scatterplot(x="pH_s0_mean", y="pH_vindta_total_20", 
+                hue="diff_opt_vindta", s=120,
+                palette="ch:r=-.2,d=.3_r", data=datac, ax=ax[2])
 
 plt.tight_layout()
