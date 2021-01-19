d684ea963fab2b4bbbb1f524735e0ed35b5e12d7
Louise Delaigue <louisedelaigue@gmail.com>
2021-01-19 10:49:55 +0100

    file init

--- pH_optode/quick_pyco2sys.py
+++ pH_optode/quick_pyco2sys.py
@@ -1,11 +1,13 @@
 import PyCO2SYS as pyco2
-CRM = pyco2.CO2SYS_nd(2212.00, 2012.59, 1, 2, 
-                      salinity=33.525,
-                      temperature_out=20,
+CRM = pyco2.CO2SYS_nd(2205.26, 2009.48, 1, 2, 
+                      salinity=33.494,
+                      temperature=20.1,
                       pressure_out=0,
-                      total_phosphate=0.42,
-                      total_silicate=3.3,
+                      total_phosphate=0.45,
+                      total_silicate=2.1,
                       opt_pH_scale=1,
                       opt_k_carbonic=16,
                       opt_total_borate=1
                       )
+
+print(CRM['pH_total_out'])
