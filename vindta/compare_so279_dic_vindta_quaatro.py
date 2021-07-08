c1c16100c19019e09530581b4580d779479b3090
Louise Delaigue <louisedelaigue@gmail.com>
2021-07-08 14:23:52 +0200

    added comparison with sal and temp

--- vindta/compare_so279_dic_vindta_quaatro.py
+++ vindta/compare_so279_dic_vindta_quaatro.py
@@ -95,21 +95,26 @@ df['density'] = calk.density.seawater_1atm_MP81(23, df['salinity'])
 df['DIC_quaatro_conv'] = df['DIC_quaatro']/df['density']
 
 # === RECALCULATE DIC TO IN-SITU TEMPERATURE
-df['DIC_quaatro_insitu'] = pyco2.sys(
-    df['TA'],
-    df['DIC_quaatro_conv'],
-    1,
-    2,
-    salinity=df['salinity'],
-    temperature=23,
-    temperature_out=df['temperature'],
-    total_phosphate=df['Phosphate'],
-    total_silicate=df['Silicate'],
-    total_ammonia=df['Ammonium']
-    )['dic']
+# df['DIC_quaatro_insitu'] = pyco2.sys(
+#     df['TA'],
+#     df['DIC_quaatro_conv'],
+#     1,
+#     2,
+#     salinity=df['salinity'],
+#     temperature=23,
+#     temperature_out=df['temperature'],
+#     total_phosphate=df['Phosphate'],
+#     total_silicate=df['Silicate'],
+#     total_ammonia=df['Ammonium']
+#     )['dic']
 
 # === COMPARE VINDTA AND QUAATRO DATA
-# === PLOTTING
+# === STATISTICS
+# Get numbers for Linear Regression 
+stats = stats.linregress(df['DIC_quaatro_conv'], df['DIC_vindta'])
+
+# ===== PLOTS
+# === LINEAR REGRESSION
 # Prepare figure
 sns.set_style('darkgrid')
 sns.set_context('paper', font_scale=1)
@@ -119,7 +124,7 @@ sns.set(font='Verdana', font_scale=1)
 fig, ax = plt.subplots(dpi=300, figsize=(6, 6))
 
 # Linear regression
-ax = sns.regplot(x='DIC_quaatro_insitu',
+ax = sns.regplot(x='DIC_quaatro_conv',
                  y='DIC_vindta',
                  data=df,
                  color='xkcd:blue',
@@ -139,10 +144,12 @@ sns.lineplot(x=x_values,
              label='Perfect fit',
              )
 
+# Add R2 to plot
+ax.text(2110, 2170, '$R^2$ = 0.96', fontsize=15)
 
 # Improve figure
-xmin = df['DIC_quaatro_insitu'].min() - 10
-xmax = df['DIC_quaatro_insitu'].max() + 10
+xmin = df['DIC_quaatro_conv'].min() - 10
+xmax = df['DIC_quaatro_conv'].max() + 10
 ymin = df['DIC_vindta'].min() - 10
 ymax = df['DIC_vindta'].max() + 10
 plt.xlim([xmin, xmax])
@@ -154,7 +161,128 @@ ax.set_xlabel('$DIC_{QuAAtro}$ / μmol/kg')
 ax.set_ylabel('$DIC_{VINDTA}$ / μmol/kg')
 
 # Save plot
-plt.savefig('./figs/compare_so279_dic_vindta_quaatro.py.png')
+plt.tight_layout()
+plt.savefig('./figs/compare_so279_dic_vindta_quaatro_linregress.png')
 
-# === STATISTICS
-stats = stats.linregress(df['DIC_quaatro_insitu'], df['DIC_vindta'])
+# === FUNCTION OF TEMPERATURE
+# Prepare figure
+sns.set_style('darkgrid')
+sns.set_context('paper', font_scale=1)
+sns.set(font='Verdana', font_scale=1)
+
+# Create figure
+fig, ax = plt.subplots(dpi=300, figsize=(7, 6))
+
+# Scatter plot
+sns.scatterplot(x='DIC_quaatro_conv',
+                 y='DIC_vindta',
+                 data=df,
+                 palette='cool',
+                 hue='temperature',
+                 legend=None,
+                 ax=ax
+                )
+
+# Adding a colorbar with seaborn
+norm = plt.Normalize(df['temperature'].min(), df['temperature'].max())
+sm = plt.cm.ScalarMappable(cmap="cool", norm=norm)
+sm.set_array([])
+ax.figure.colorbar(sm, label='In-situ temperature / $^°$C')
+
+# Line through origin
+x = [2000, 2300]
+y = [2000, 2300]
+x_values = [x[0], x[1]]
+y_values = [y[0], y[1]]
+pf = sns.lineplot(x=x_values,
+             y=y_values,
+             ax=ax,
+             linestyle='--',
+             color='black',
+             label='Perfect fit'
+             )
+
+ax.legend(loc='upper left')
+
+# Improve figure
+xmin = df['DIC_quaatro_conv'].min() - 10
+xmax = df['DIC_quaatro_conv'].max() + 10
+ymin = df['DIC_vindta'].min() - 10
+ymax = df['DIC_vindta'].max() + 10
+plt.xlim([xmin, xmax])
+plt.ylim([ymin, ymax])
+
+ax.set_xlabel('$DIC_{QuAAtro}$ / μmol/kg')
+ax.set_ylabel('$DIC_{VINDTA}$ / μmol/kg')
+
+# Save plot
+plt.tight_layout()
+plt.savefig('./figs/compare_so279_dic_vindta_quaatro_temp.png')
+
+## === FUNCTION OF SALINITY
+# Prepare figure
+sns.set_style('darkgrid')
+sns.set_context('paper', font_scale=1)
+sns.set(font='Verdana', font_scale=1)
+
+# Create figure
+fig, ax = plt.subplots(dpi=300, figsize=(7, 6))
+
+# Scatter plot
+sns.scatterplot(x='DIC_quaatro_conv',
+                 y='DIC_vindta',
+                 data=df,
+                 palette='cool',
+                 hue='salinity',
+                 legend=None,
+                 ax=ax
+                )
+
+# Adding a colorbar with seaborn
+norm = plt.Normalize(df['salinity'].min(), df['salinity'].max())
+sm = plt.cm.ScalarMappable(cmap="cool", norm=norm)
+sm.set_array([])
+ax.figure.colorbar(sm, label='Salinity')
+
+# Line through origin
+x = [2000, 2300]
+y = [2000, 2300]
+x_values = [x[0], x[1]]
+y_values = [y[0], y[1]]
+pf = sns.lineplot(x=x_values,
+             y=y_values,
+             ax=ax,
+             linestyle='--',
+             color='black',
+             label='Perfect fit'
+             )
+ax.legend(loc='upper left')
+
+# Improve figure
+xmin = df['DIC_quaatro_conv'].min() - 10
+xmax = df['DIC_quaatro_conv'].max() + 10
+ymin = df['DIC_vindta'].min() - 10
+ymax = df['DIC_vindta'].max() + 10
+plt.xlim([xmin, xmax])
+plt.ylim([ymin, ymax])
+
+ax.set_xlabel('$DIC_{QuAAtro}$ / μmol/kg')
+ax.set_ylabel('$DIC_{VINDTA}$ / μmol/kg')
+
+# Save plot
+plt.tight_layout()
+plt.savefig('./figs/compare_so279_dic_vindta_quaatro_sal.png')
+
+# === SAVE DF INCLUDING ONLY TEMP, SAL AND DIC
+dic_data = pd.DataFrame()
+dic_data['Latitude'] = df['Latitude']
+dic_data['Longitude'] = df['Longitude']
+dic_data['Station'] = df['station']
+dic_data['Niskin'] = df['niskin']
+dic_data['Temperature'] = df['temperature']
+dic_data['Salinity'] = df['salinity']
+dic_data['DIC_VINDTA'] = df['DIC_vindta']
+dic_data['DIC_QUAATRO'] = df['DIC_quaatro']
+
+# Save to .csv
+dic_data.to_csv('./data/dic_data_comparison.csv', index=False)
