6e515f45d3b96fbb5cd47da54b5d553910a10011
Louise Delaigue <louisedelaigue@gmail.com>
2021-10-14 11:53:46 +0200

    formatting

--- airica/storage_test_estimate_pco2.py
+++ airica/storage_test_estimate_pco2.py
@@ -5,16 +5,16 @@ pCO2_12C = pyco2.sys(2281.91,
                     1,
                     2,
                     salinity=32,
-                    temperature=25,
+                    temperature=23.46,
                     temperature_out=12.5,
                       )['pCO2_out']
 
 pCO2_4C = pyco2.sys(2281.91,
-                     2167.865,
-                     1,
-                     2,
-                     salinity=32,
-                     temperature=23.46,
-                     temperature_out=4,
-                       )['pCO2_out']
+                    2167.865,
+                    1,
+                    2,
+                    salinity=32,
+                    temperature=23.46,
+                    temperature_out=4,
+                      )['pCO2_out']
 
