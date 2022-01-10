47f61a1507f2b642be4ea0bd7b528de172444d99
louisedelaigue <louisedelaigue@gmail.com>
2022-01-10 16:58:12 +0100

    remove acid drift correction + treat each analysis batch as one acid batch

---
+++ vindta/acid_drift_polyfit.py
@@ -0,0 +1,67 @@
+# TO ADD AFTER
+# /// Calibrate and solve alkalinity and plot calibration ///
+
+# === REMEDY ACID DRIFT
+# Isolate dbs in new pandas DataFrame
+L = (dbs["bottle"].str.startswith("CRM-")) & (dbs["reference_good"]==True)
+dbs_crms = dbs[L] 
+
+# Apply np.polytfit (order 3) to CRM titrant molinity
+p = np.polyfit(dbs_crms["analysis_datenum"],
+                dbs_crms["titrant_molinity_here"],
+                3)
+
+f = np.poly1d(p)
+
+# Apply fit to data
+x_new = np.linspace(dbs["analysis_datenum"].min(),
+                    dbs["analysis_datenum"].max(),
+                    100)
+y_new = f(x_new)
+
+# Plot fit
+# Create figure
+fig, ax = plt.subplots(dpi=300)
+
+# Plot fit
+sns.lineplot(x=x_new,
+              y=y_new,
+              color='xkcd:blue',
+              label="Polynomial fit (deg 3)",
+              ax=ax)
+
+# Scatter titrant molinity from CRMs
+sns.scatterplot(x="analysis_datenum",
+                y="titrant_molinity_here",
+                data=dbs_crms,
+                color='xkcd:blue',
+                label="CRMs titrant molinity",
+                ax=ax)
+
+# Apply fit to titrant molinity from CRMs
+dbs_crms["titrant_molinity_polyfit"] = f(dbs_crms["analysis_datenum"])
+
+# Take the mean from the polyfit titrant molinities
+molinity_coefficient = dbs_crms["titrant_molinity_polyfit"].mean()
+
+# Update titrant molity value in dbs file
+# dbs["titrant_molinity"] = f(dbs["analysis_datenum"])
+# dbs["titrant_molinity"] = molinity_coefficient
+
+# Update titrant molinity value in dbs file
+# analysis_dates = dbs["analysis_datetime"].dt.day.unique().tolist()
+# for date in analysis_dates:
+#     A = dbs["analysis_datetime"].dt.day==date
+#     B = dbs_crms["analysis_datetime"].dt.day==date
+#     dbs.loc[A, "titrant_molinity"] = dbs_crms.loc[B, "titrant_molinity_polyfit"].mean()
+
+analysis_batch = dbs["analysis_batch"].unique().tolist()
+for batch in analysis_batch:
+    A = dbs["analysis_batch"]==batch
+    B = dbs_crms["analysis_batch"]==batch
+    dbs.loc[A, "titrant_molinity"] = dbs_crms.loc[B, "titrant_molinity_polyfit"].mean()
+
+# dbs.loc[dbs['analysis_datetime'].dt.day==6,'titrant_molinity'] = 0.099252498
+# dbs.loc[dbs['analysis_datetime'].dt.day==13, 'titrant_molinity'] = 0.099252528
+# dbs.loc[dbs['analysis_datetime'].dt.day==27, 'titrant_molinity'] = 0.099252474
+# dbs.loc[dbs['analysis_datetime'].dt.day==18, 'titrant_molinity'] = 0.099252388
--- vindta/storage_test_process.py
+++ vindta/storage_test_process.py
@@ -167,9 +167,9 @@ dbs["file_path"] = "data/LD_storage_test_TA/"
 # first 4 analysis had the same bach made 15/09/2021
 # // this remedies the acid drift
 dbs['analysis_batch'] = 1 # made 15/09/2021
-dbs.loc[dbs['analysis_datetime'].dt.day==13, 'analysis_batch'] = 1
-dbs.loc[dbs['analysis_datetime'].dt.day==27, 'analysis_batch'] = 2
-dbs.loc[dbs['analysis_datetime'].dt.day==18, 'analysis_batch'] = 3
+dbs.loc[dbs['analysis_datetime'].dt.day==13, 'analysis_batch'] = 2
+dbs.loc[dbs['analysis_datetime'].dt.day==27, 'analysis_batch'] = 3
+dbs.loc[dbs['analysis_datetime'].dt.day==18, 'analysis_batch'] = 4
 
 # Select which TA CRMs to use/avoid for calibration
 dbs["reference_good"] = ~np.isnan(dbs.alkalinity_certified)
@@ -186,71 +186,6 @@ dbs.loc[dbs['bottle']=='CRM-189-0159-3', 'file_good'] = False
 # Calibrate and solve alkalinity and plot calibration
 calk.io.get_VINDTA_filenames(dbs)
 calk.dataset.calibrate(dbs)
-
-# === REMEDY ACID DRIFT
-# Isolate dbs in new pandas DataFrame
-L = (dbs["bottle"].str.startswith("CRM-")) & (dbs["reference_good"]==True)
-dbs_crms = dbs[L] 
-
-# Apply np.polytfit (order 3) to CRM titrant molinity
-p = np.polyfit(dbs_crms["analysis_datenum"],
-                dbs_crms["titrant_molinity_here"],
-                3)
-
-f = np.poly1d(p)
-
-# Apply fit to data
-x_new = np.linspace(dbs["analysis_datenum"].min(),
-                    dbs["analysis_datenum"].max(),
-                    100)
-y_new = f(x_new)
-
-# Plot fit
-# Create figure
-fig, ax = plt.subplots(dpi=300)
-
-# Plot fit
-sns.lineplot(x=x_new,
-              y=y_new,
-              color='xkcd:blue',
-              label="Polynomial fit (deg 3)",
-              ax=ax)
-
-# Scatter titrant molinity from CRMs
-sns.scatterplot(x="analysis_datenum",
-                y="titrant_molinity_here",
-                data=dbs_crms,
-                color='xkcd:blue',
-                label="CRMs titrant molinity",
-                ax=ax)
-
-# Apply fit to titrant molinity from CRMs
-dbs_crms["titrant_molinity_polyfit"] = f(dbs_crms["analysis_datenum"])
-
-# Take the mean from the polyfit titrant molinities
-molinity_coefficient = dbs_crms["titrant_molinity_polyfit"].mean()
-
-# Update titrant molity value in dbs file
-# dbs["titrant_molinity"] = f(dbs["analysis_datenum"])
-# dbs["titrant_molinity"] = molinity_coefficient
-
-# Update titrant molinity value in dbs file
-# analysis_dates = dbs["analysis_datetime"].dt.day.unique().tolist()
-# for date in analysis_dates:
-#     A = dbs["analysis_datetime"].dt.day==date
-#     B = dbs_crms["analysis_datetime"].dt.day==date
-#     dbs.loc[A, "titrant_molinity"] = dbs_crms.loc[B, "titrant_molinity_polyfit"].mean()
-
-analysis_batch = dbs["analysis_batch"].unique().tolist()
-for batch in analysis_batch:
-    A = dbs["analysis_batch"]==batch
-    B = dbs_crms["analysis_batch"]==batch
-    dbs.loc[A, "titrant_molinity"] = dbs_crms.loc[B, "titrant_molinity_polyfit"].mean()
-
-# dbs.loc[dbs['analysis_datetime'].dt.day==6,'titrant_molinity'] = 0.099252498
-# dbs.loc[dbs['analysis_datetime'].dt.day==13, 'titrant_molinity'] = 0.099252528
-# dbs.loc[dbs['analysis_datetime'].dt.day==27, 'titrant_molinity'] = 0.099252474
-# dbs.loc[dbs['analysis_datetime'].dt.day==18, 'titrant_molinity'] = 0.099252388
 
 # == CONTINUE SOLVING
 titrant_molinity_fname = "figs/LD_storage_test_TA/titrant_molinity.png"
