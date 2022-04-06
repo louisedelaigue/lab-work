78d3c8c91d883635621352f9106030130dd91db6
Louise Delaigue <louisedelaigue@gmail.com>
2022-04-06 15:47:47 +0200

    Create calc_pH_change_temp.py

---
+++ pH_optode/calc_pH_change_temp.py
@@ -0,0 +1,10 @@
+import PyCO2SYS as pyco2
+
+
+results = pyco2.sys(par1=8.1,
+                    par2=450,
+                    par1_type=3,
+                    par2_type=4,
+                    temperature=25,
+                    temperature_out=30
+                    )
